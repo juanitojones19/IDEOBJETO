2257029866b02c08462fd3db380a69f10e8b2094
juanitojones19 <hastarm@gmail.com>
2015-09-28 03:23:10 -0500

    SE modifacon coas menores

--- IDEOBJETO/src/com/Traductores/Views/AgregarMetodos.java
+++ IDEOBJETO/src/com/Traductores/Views/AgregarMetodos.java
@@ -48,7 +48,7 @@ public class AgregarMetodos extends javax.swing.JDialog {
         botonTerminar = new javax.swing.JButton();
         botonCancelar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Nombre del metodo:");
 
