1287bec367ea5391df59d964064214a5055a07f2
juanitojones19 <hastarm@gmail.com>
2015-09-28 18:24:06 -0500

    Se agrego la calse para interactuar entre la variables

--- IDEOBJETO/src/com/Traductores/Controllers/VariableController.java
+++ IDEOBJETO/src/com/Traductores/Controllers/VariableController.java
@@ -9,50 +9,43 @@ package com.Traductores.Controllers;
 import com.Traductores.Models.Diccionario;
 import com.Traductores.Models.VariableModel;
 import com.Traductores.Views.AgregarAtributosVista;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /**
  *
  * @author JC
  */
 public class VariableController {
     AgregarAtributosVista vista;
+    private String nombrevariable;
+    private String tipoDato;
+    private String valor;
+    private String acceso;
    
     public VariableController(AgregarAtributosVista vista)
     {
         this.vista = vista;
-        this.vista.agregarAtributosListener(new AgregarAtributosListener());
+        this.acceso = vista.getjComboBox1().getSelectedItem().toString();
+        this.nombrevariable = vista.getTextFielNombreVariable().getText();
+        this.tipoDato = vista.getTextFieldTipoDato().getText();
+        this.valor = vista.getTextFieldValor().getText();
         
+        //this.vista.mostarMensajeError("nop");
+        
+        validarAtributos(this.nombrevariable, this.tipoDato, this.valor);
     }
     
-    class AgregarAtributosListener implements ActionListener{
+    public void validarAtributos(String nombreVariable, String tipoDato, String valor){
+        System.out.println(vista.getjComboBox1().getSelectedItem());
+        if(VariableModel.validar(nombreVariable, tipoDato))
+        {
+            vista.mostarMensajeError("Exito");
+            VariableModel.agregarVariable(this.acceso, nombreVariable, tipoDato, valor);
+        }else{
+            vista.mostarMensajeError(VariableModel.obtenerMensaje());
+        }
+    }// fin del metodo validarAtributos
 
-        @Override
-        public void actionPerformed(ActionEvent ae) {
-            
-            /*
-            if(VariableModel.esNombreValido(vista.getTextFielNombreVariable().getText())){
-                if (!VariableModel.esPalabraReservada(vista.getTextFielNombreVariable().getText())) {
-                    VariableModel.agregarVariable(vista.getTextFielNombreVariable().getText(), vista.getTextFieldTipoDato().getText(), vista.getTextFieldValor().getText());
-                }else{//es palabra reservada
-                    vista.mostarMensajeError(Diccionario.obtenerListaPalabras().get(vista.getTextFielNombreVariable().getText()));
-                }
-            }else{//no es nombre valido
-                vista.mostarMensajeError("No es nombre valido");
-                
-            }// fin if else
-            */
-            if(VariableModel.validar(vista.getTextFielNombreVariable().getText(), vista.getTextFieldTipoDato().getText()))
-            {
-                vista.mostarMensajeError(VariableModel.obtenerMensaje());
-                VariableModel.agregarVariable(vista.getTextFielNombreVariable().getText(), vista.getTextFieldTipoDato().getText(), vista.getTextFieldValor().getText());
-            }else{
-                vista.mostarMensajeError(VariableModel.obtenerMensaje());
-            }
-            System.out.println("Botno");
-        }//fin metodo actionPerformed
+
     
-    }//fin de la clase interna
+    
     
 }
