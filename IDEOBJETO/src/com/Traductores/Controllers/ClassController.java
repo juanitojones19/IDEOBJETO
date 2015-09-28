0a47077628e9f528a164c0689f99742ff246890c
juanitojones19 <hastarm@gmail.com>
2015-09-28 03:21:17 -0500

    Se intento agregar un listenr, no se ha logrado

--- IDEOBJETO/src/com/Traductores/Controllers/ClassController.java
+++ IDEOBJETO/src/com/Traductores/Controllers/ClassController.java
@@ -6,10 +6,80 @@
 
 package com.Traductores.Controllers;
 
+import com.Traductores.Models.*;
+import com.Traductores.Views.AgregarAtributosVista;
+import com.Traductores.Views.NuevoArchivo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.tools.Diagnostic;
+
 /**
  *
  * @author JC
  */
-public class ClassController {
+public class ClassController 
+{
+    private static String mensaje;
+    private static String nombreClase;
+    private NuevoArchivo vista;
+    private final String mensajeErrorIdentificadorNoValido = "El nombre no es un identificador valido";
+    private final String mensajeErrorPalabraReservada = "El nombre es una palabra reservada";
+    
+    
+    public ClassController(NuevoArchivo vista, String nombreClase)
+    {
+        this.nombreClase = nombreClase;
+        this.vista = vista;
+        //Diccionario.cargarPalabras("palabrasReservadas.txt");
+        validarNombre(nombreClase);
+        this.vista.agregarAtribustListener(new AgregarListener());
+    }
+    
+    public void validarNombre(String nombreClase)
+    {
+        if(!ClassModel.esPalabraReservada(nombreClase))// si no es palabra reservada
+        {
+            if(ClassModel.esNombreValido(nombreClase))// si el nombre es valido se genra el XML
+            {
+                ArchivoXML.crearDocumentoXML();
+                ArchivoXML.crearTagClase(nombreClase);
+                vista.activarBotonTerminar(true);
+                vista.activarBotonSiguiente(true);
+            }else{// si el no nombre es valido
+                vista.mensaje(mensajeErrorIdentificadorNoValido);
+            }
+        }else{//si es palabra reservada
+            //Diccionario.cargarPalabras("palabrasReservadas.txt");
+            vista.mensaje(Diccionario.obtenerListaPalabras().get(nombreClase));
+        }
+    }
+    
+    public static String getMensaje()
+    {
+        return mensaje;
+    }
+    
+    public static void GuardarXML(String ruta)
+    {
+        ArchivoXML.contendioXML(ruta);
+    }
+    
+    class AgregarListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            AgregarAtributosVista vistaAtributos = new AgregarAtributosVista(vista, true);
+            vistaAtributos.setVisible(true);
+            vistaAtributos.setModal(true);
+            VariableController controladorAtributos = new VariableController(vistaAtributos);
+            
+        }
+    
+    }
     
 }
+
+
+
+
+
